5111ea4ac43cb8f13dbd6638f836cc3cb330d942
Princess-Bermudez <98816541+Princess-Bermudez@users.noreply.github.com>
2022-02-25 13:24:02 +0800

    Update index.php

--- index.php
+++ index.php
@@ -4,5 +4,14 @@
  <link rel="icon" href="9d g22.png" type="image/icon type">
 </head> 
 <body background="webpg bg.jpg">
+<center>
+<h2> <font color="white"> Third Quarter Performance Task- Message of HOPE </font></h2> 
+<h3><font color="white"> "Place of Hope" </font></h3>
+<img src="place of hope.png "width="465" height="265" hspace="8"> 
+<a href="https://youtu.be/rHKkJ0LKBVY">
+<br> 
+<br> 
+Click here to see video
+</a>
 </body> 
 </html> 
