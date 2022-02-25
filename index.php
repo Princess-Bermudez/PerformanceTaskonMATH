ad4caa24a5632ef2f46e33f609a561d49631a160
Princess-Bermudez <98816541+Princess-Bermudez@users.noreply.github.com>
2022-02-25 14:27:53 +0800

    Update index.php

--- index.php
+++ index.php
@@ -3,9 +3,9 @@
 <title> MATHEMATICS </title> 
  <link rel="icon" href="9d g22.png" type="image/icon type">
 </head> 
-<body background="webpg bg.jpg">
+<body background="math bg.png">
 <center>
-<h2> <font color="white"> Third Quarter Performance Task- Message of HOPE </font></h2> 
+<h2> <font face="Calibri"> Third Quarter Performance Task- Message of HOPE </font></h2> 
 <h3><font color="white"> "Place of Hope" </font></h3>
 <img src="place of hope.png "width="465" height="265" hspace="8"> 
 <a href="https://youtu.be/rHKkJ0LKBVY">
